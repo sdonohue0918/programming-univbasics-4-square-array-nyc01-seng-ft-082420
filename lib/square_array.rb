947b7e815436e63b921b17a591b9bc94827c642b
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-22 22:54:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
+
+
+
 def square_array(array)
-  # your code here
+  new_numbers = []
+  counter = 0
+  while counter < array.length do
+    new_numbers.push array[counter] ** 2
+    counter += 1
+  end
+  new_numbers
 end
+
+yung_dingus = [1, 2, 3]
+
+
+square_array(yung_dingus)
